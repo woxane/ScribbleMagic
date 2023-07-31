ea711c08d890731ce5a5fef07ebc08a52927b942
woxane <mohsen.minavand1384@gmail.com>
2023-07-31 08:59:00 +0330

    write instruction of persian language

---
+++ main.py
@@ -0,0 +1,7 @@
+#Instruction of Persian alphabet that i consider them as numbers (0 to 3) 
+#Some letter of the Persian alphabet has four forms :
+#Unconnected = 0 , Like = ب , ت 
+#Followed by another letter = 1 , Like = ــپ , ــچ 
+#Middle = 2 , Like = ــبــ , ـجــ 
+#End of Word = 3 , Like = جــ , خــ
+#But there is exception for these letter ( some of them have two or three forms ) 
